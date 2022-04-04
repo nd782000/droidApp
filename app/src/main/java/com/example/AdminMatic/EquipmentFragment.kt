064ed681adb92823d964273f8eb6654935a6802e
nd782000 <nick@atlanticlawnandgarden.com>
2022-04-04 09:58:31 -0400

    Update EquipmentFragment.kt
    Combined the two recycler views into one with separate adapters depending on state.  Can eliminate the second (history) adapter file.  It does the same thing as the other adapter so no need for two.

--- app/src/main/java/com/example/AdminMatic/EquipmentFragment.kt
+++ app/src/main/java/com/example/AdminMatic/EquipmentFragment.kt
@@ -52,8 +52,14 @@ class EquipmentFragment : Fragment(), ServiceCellClickListener {
     lateinit var myView:View
 
     lateinit var  pgsBar: ProgressBar
-    lateinit var currentRecyclerView: RecyclerView
-    lateinit var historyRecyclerView: RecyclerView
+
+    lateinit var serviceRecyclerView: RecyclerView
+
+    lateinit var currentServicesAdapter:ServiceAdapter
+    lateinit var historyServicesAdapter:ServiceAdapter
+
+    //lateinit var currentRecyclerView: RecyclerView
+    //lateinit var historyRecyclerView: RecyclerView
 
     lateinit var equipmentImageView: ImageView
     lateinit var nameTxt:TextView
@@ -62,6 +68,7 @@ class EquipmentFragment : Fragment(), ServiceCellClickListener {
     lateinit var detailsBtn:Button
 
     lateinit var tabLayout: TabLayout
+    lateinit var tableMode:String
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -97,8 +104,12 @@ class EquipmentFragment : Fragment(), ServiceCellClickListener {
 
 
         pgsBar = view.findViewById(R.id.progress_bar)
-        currentRecyclerView = view.findViewById(R.id.service_recycler_view)
-        historyRecyclerView = view.findViewById(R.id.service_history_recycler_view)
+
+        serviceRecyclerView = view.findViewById(R.id.service_recycler_view)
+
+
+       // currentRecyclerView = view.findViewById(R.id.service_recycler_view)
+       // historyRecyclerView = view.findViewById(R.id.service_history_recycler_view)
         nameTxt = myView.findViewById(R.id.equipment_name_txt)
         typeTxt = myView.findViewById(R.id.equipment_type_txt)
         crewTxt = myView.findViewById(R.id.equipment_crew_txt)
@@ -138,16 +149,23 @@ class EquipmentFragment : Fragment(), ServiceCellClickListener {
             override fun onTabSelected(tab: TabLayout.Tab?) {
                 when (tab!!.position) {
                     0 -> {
+                        tableMode = "CURRENT"
+
+                        serviceRecyclerView.adapter = currentServicesAdapter
+                       // serviceRecyclerView
                         //tableMode = "LEADS"
                         // Toast.makeText(com.example.AdminMatic.myView.context, "Leads", Toast.LENGTH_SHORT).show()
-                        currentRecyclerView.visibility = View.VISIBLE
-                        historyRecyclerView.visibility = View.GONE
+                        //currentRecyclerView.visibility = View.VISIBLE
+                       // historyRecyclerView.visibility = View.GONE
                     }
                     1 -> {
+
+                        serviceRecyclerView.adapter = historyServicesAdapter
+                        tableMode = "HISTORY"
                         //tableMode = "CONTRACTS"
                         //Toast.makeText(com.example.AdminMatic.myView.context, "Contracts", Toast.LENGTH_SHORT).show()
-                        currentRecyclerView.visibility = View.GONE
-                        historyRecyclerView.visibility = View.VISIBLE
+                        //currentRecyclerView.visibility = View.GONE
+                        //historyRecyclerView.visibility = View.VISIBLE
                     }
                 }
             }
@@ -157,11 +175,122 @@ class EquipmentFragment : Fragment(), ServiceCellClickListener {
             }
         })
 
-        getServicesCurrent()
+        //getServicesCurrent()
+
+        tableMode = "CURRENT"
+        getServiceInfo()
 
     }
 
 
+
+    fun getServiceInfo(){
+        println("getServiceInfo")
+
+
+        showProgressView()
+
+        var urlString = "https://www.adminmatic.com/cp/app/functions/get/equipment.php"
+
+        val currentTimestamp = System.currentTimeMillis()
+        println("urlString = ${"$urlString?cb=$currentTimestamp"}")
+        urlString = "${"$urlString?cb=$currentTimestamp"}"
+        val queue = Volley.newRequestQueue(myView.context)
+
+        val postRequest1: StringRequest = object : StringRequest(
+            Method.POST, urlString,
+            Response.Listener { response -> // response
+                println("Response $response")
+                hideProgressView()
+                try {
+
+                    val gson = GsonBuilder().create()
+
+
+                    val parentObject = JSONObject(response)
+                    println("parentObject = ${parentObject.toString()}")
+
+                    //current adapter
+                    var services:JSONArray = parentObject.getJSONArray("services")
+                    println("services = ${services.toString()}")
+                    println("services count = ${services.length()}")
+
+                    val servicesListCurrent = gson.fromJson(services.toString() , Array<EquipmentService>::class.java).toMutableList()
+                    println("ServiceCount = ${servicesListCurrent.count()}")
+
+                    currentServicesAdapter = ServiceAdapter(servicesListCurrent,this.myView.context,this)
+
+                    //history adapter
+                    var servicesHistory:JSONArray = parentObject.getJSONArray("serviceHistory")
+                    println("servicesHistory = ${servicesHistory.toString()}")
+                    println("servicesHistory count = ${servicesHistory.length()}")
+
+                    val servicesListHistory = gson.fromJson(servicesHistory.toString() , Array<EquipmentService>::class.java).toMutableList()
+                    println("ServiceHistoryCount = ${servicesListHistory.count()}")
+
+                    historyServicesAdapter = ServiceAdapter(servicesListHistory,this.myView.context,this)
+
+
+                    serviceRecyclerView.layoutManager = LinearLayoutManager(this.myView.context, RecyclerView.VERTICAL, false)
+
+                    //currentRecyclerView.layoutManager = LinearLayoutManager(this.myView.context, RecyclerView.VERTICAL, false)
+
+
+
+                    serviceRecyclerView.adapter = currentServicesAdapter
+
+
+
+
+                    val itemDecoration: RecyclerView.ItemDecoration =
+                        DividerItemDecoration(myView.context, DividerItemDecoration.VERTICAL)
+                    serviceRecyclerView.addItemDecoration(itemDecoration)
+
+                    /*
+                    currentRecyclerView.apply {
+                        layoutManager = LinearLayoutManager(activity)
+                        adapter = activity?.let {
+                            ServiceAdapter(servicesList,
+                                it, this@EquipmentFragment)
+                        }
+                        (adapter as ServiceAdapter).notifyDataSetChanged();
+                    }
+
+                     */
+
+                    /* Here 'response' is a String containing the response you received from the website... */
+
+                   // getServicesHistory()
+
+
+                } catch (e: JSONException) {
+                    println("JSONException")
+                    e.printStackTrace()
+                }
+            },
+            Response.ErrorListener { // error
+                //Log.e("VOLLEY", error.toString())
+            }
+        ) {
+            override fun getParams(): Map<String, String> {
+                val params: MutableMap<String, String> = HashMap()
+                if (equipment != null){
+                    params["equipmentID"] = equipment!!.ID
+                }
+                params["companyUnique"] = GlobalVars.loggedInEmployee!!.companyUnique
+                params["sessionKey"] = GlobalVars.loggedInEmployee!!.sessionKey
+                println("params = ${params.toString()}")
+                return params
+            }
+        }
+        queue.add(postRequest1)
+
+
+
+    }
+
+
+    /*
     fun getServicesCurrent() {
 
         println("getServicesCurrent")
@@ -302,22 +431,32 @@ class EquipmentFragment : Fragment(), ServiceCellClickListener {
         }
         queue.add(postRequest1)
     }
+*/
 
 
     override fun onServiceCellClickListener(data:EquipmentService){
         println("onServiceCellClickListener ${data.ID}")
+        if (tableMode == "CURRENT"){
+            println("Show service in current mode")
+        }else{
+            println("Show service in history mode")
+        }
     }
+
+
 
     fun showProgressView() {
         pgsBar.visibility = View.VISIBLE
-        currentRecyclerView.visibility = View.INVISIBLE
-        historyRecyclerView.visibility = View.INVISIBLE
+        serviceRecyclerView.visibility = View.INVISIBLE
+        //currentRecyclerView.visibility = View.INVISIBLE
+       // historyRecyclerView.visibility = View.INVISIBLE
     }
 
     fun hideProgressView() {
         pgsBar.visibility = View.INVISIBLE
-        currentRecyclerView.visibility = View.VISIBLE
-        historyRecyclerView.visibility = View.VISIBLE
+        serviceRecyclerView.visibility = View.VISIBLE
+       // currentRecyclerView.visibility = View.VISIBLE
+       // historyRecyclerView.visibility = View.VISIBLE
     }
 
     companion object {
